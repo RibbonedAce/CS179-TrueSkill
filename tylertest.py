f7d0aa94509b96ffa37dc52e0df1b323f3c5bb60
Tyler Gemora <tylergemora@yahoo.com>
2019-05-30 16:10:18 -0700

    Match data

--- tylertest.py
+++ tylertest.py
@@ -1,5 +1,7 @@
 import time
 import datetime
+import csv
+
 from trueskill import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -36,33 +38,46 @@ def input_values(driver, inputs, player1, player2):
     inputs[4].send_keys("31/12/2018")
     inputs[4].submit()
 
-def get_matches(driver, matches):
+def get_matches(driver, matches, player1, player2):
     match_table = None
     try:
         match_table = driver.find_element_by_class_name("table-responsive").find_elements_by_xpath("./*")[0].find_elements_by_xpath("./*")[0].find_elements_by_xpath("./*")
     except:
         return
-    date = None
-    for k in range(len(match_table)):
+    in_match = False
+    match_queue = []
+    for k in range(len(match_table)-1, -1, -1):
         if match_table[k].get_attribute("class") == "match-row":
+            in_match = True
             set_count = match_table[k].find_elements_by_xpath("./*")[1:3]
             set_count = (int(set_count[0].text), int(set_count[1].text))
+            print(player1, player2, set_count)
             if set_count[0] > set_count[1]:
-                set_count = (i, j)
+                set_count = (player1, player2)
             else:
-                set_count = (j, i)
-            if not date:
-                date = date_from_text(match_table[k-1].find_elements_by_xpath("./*")[0].text)
-            matches.append((set_count, date))
-        else:
-            date = None
+                set_count = (player2, player1)
+            match_queue.append(set_count)
+        elif in_match:
+            in_match = False
+            date = date_from_text(match_table[k].find_elements_by_xpath("./*")[0].text)
+            for match in match_queue:
+                matches.append((match[0], match[1], date))
+            match_queue = []
+
+def store_matches(matches, file_name="matches.csv"):
+    with open(file_name, mode="w", newline="") as match_file:
+        writer = csv.writer(match_file, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
+        matches.sort(key=lambda x: max(x[0], x[1]), reverse=True)
+        matches.sort(key=lambda x: min(x[0], x[1]), reverse=True)
+        matches.sort(key=lambda x: x[2])
+        writer.writerows(matches)
 
 def apply_matches(matches, ratings):
-    matches.sort(key=lambda x: x[1])
+    matches.sort(key=lambda x: x[2])
     for match in matches:
-        new_r1, new_r2 = rate_1vs1(ratings[match[0][0]], ratings[match[0][1]])
-        ratings[match[0][0]] = new_r1
-        ratings[match[0][1]] = new_r2
+        new_r1, new_r2 = rate_1vs1(ratings[match[0]], ratings[match[0]])
+        ratings[match[0]] = new_r1
+        ratings[match[1]] = new_r2
 
 def date_from_text(text):
     attr = text.split("-")
@@ -85,9 +100,10 @@ if __name__ == "__main__":
         for j in range(i+1, len(ratings)):
             inputs = get_inputs(driver)
             input_values(driver, inputs, players[i], players[j])
-            get_matches(driver, matches)         
+            get_matches(driver, matches, i, j)         
     driver.quit()
-    
+
+    store_matches(matches)
     apply_matches(matches, ratings)
     print(dict(zip(players, ratings)))
 
