b7dc0ee2fb4ff68eb5a6ea00c5fdd7667c8645c7
Tyler Gemora <tylergemora@yahoo.com>
2019-06-01 10:18:17 -0700

    Top 100 player database

--- tylertest.py
+++ tylertest.py
@@ -10,7 +10,25 @@ import os
     
 read_from_csv = True
 players = ["Hungrybox", "Armada", "Leffen", "Plup", "Mango", \
-           "Mew2King", "Zain", "Wizzrobe", "aMSa", "Axe"]
+           "Mew2King", "Zain", "Wizzrobe", "aMSa", "Axe", \
+           "S2J", "SFAT", "Swedish Delight", "Duck", "PewPewU", \
+           "n0ne", "Crush", "Lucky", "Bananas", "ARMY", \
+           "lloD", "Westballz", "HugS", "AbsentPage", "KJH", \
+           "Rishi", "Gahtzu", "Fiction", "Shroomed", "Colbol", \
+           "Ryan Ford", "La Luna", "Captain Faceroll", "iBDW", "Trif", \
+           "Syrox", "Kalamazhu", "Hax", "Ice", "Ginger", \
+           "Michael", "Captain Smuckers", "MikeHaze", "Bladewise", "Junebug", \
+           "ChuDat", "Spud", "Santi", "2saint", "Slox", \
+           "Rocky (American Player)", "moky", "Spark", "Squid", "Jerry", \
+           "Professor Pro", "Overtriforce", "Ka-Master", "Darktooth", "Drephen", \
+           "Drunksloth", "Stango", "Abate", "Amsah", "Nintendude", \
+           "Android", "Boyd", "42nd", "FatGoku", "Sharkz", \
+           "Cactuar", "Tai", "Kels", "DaShizWiz", "Darkatma", \
+           "Eddy Mexico", "Prince Abu", "Zamu", "Kalvar", "Uncle Mojo", \
+           "HTwa", "Rik", "NMW", "King Momo", "Legend", \
+           "Smashdaddy", "Cal", "Jakenshaken", "Ralph", "Flipsy", \
+           "TheRealThing", "MilkMan", "Trulliam", "Cob", "Iceman", \
+           "vortex", "Magi", "nebbii", "Morsecode762", "Azel"]
 
 def get_driver():
     result = None
@@ -20,7 +38,7 @@ def get_driver():
         result = webdriver.Chrome(cwd)
     else:
         result = webdriver.Chrome()
-    result.implicitly_wait(30)
+    result.implicitly_wait(15)
     return result
 
 def get_inputs(driver):
@@ -32,13 +50,8 @@ def get_inputs(driver):
     inputs[4] = inputs[4].find_element_by_class_name("hasDatepicker")
     return inputs
 
-def input_values(driver, inputs, player1, player2):
+def input_values(driver, inputs, player1):
     inputs[0].send_keys(players[i])
-    time.sleep(1)
-    ActionChains(driver).send_keys(Keys.ENTER).perform()
-    ActionChains(driver).send_keys(Keys.ENTER).perform()
-
-    inputs[1].send_keys(players[j])
     time.sleep(1)
     ActionChains(driver).send_keys(Keys.ENTER).perform()
     ActionChains(driver).send_keys(Keys.ENTER).perform()
@@ -49,7 +62,7 @@ def input_values(driver, inputs, player1, player2):
     inputs[4].send_keys("31/12/2018")
     inputs[4].submit()
 
-def get_matches(driver, matches, player1, player2):
+def get_matches(driver, matches, player1):
     match_table = None
     try:
         match_table = driver.find_element_by_class_name("table-responsive").find_elements_by_xpath("./*")[0].find_elements_by_xpath("./*")[0].find_elements_by_xpath("./*")
@@ -60,13 +73,24 @@ def get_matches(driver, matches, player1, player2):
     for k in range(len(match_table)-1, -1, -1):
         if match_table[k].get_attribute("class") == "match-row":
             in_match = True
-            set_count = match_table[k].find_elements_by_xpath("./*")[1:3]
-            set_count = (int(set_count[0].text), int(set_count[1].text))
-            print(player1, player2, set_count)
-            if set_count[0] > set_count[1]:
+            set_count = match_table[k].find_elements_by_xpath("./*")
+            opponent = set_count[3].text
+            if not opponent in players:
+                continue
+            player2 = players.index(opponent)
+            if player2 <= player1:
+                continue
+            set_count = set_count[1:3]
+            if set_count[0].text == "W":
                 set_count = (player1, player2)
-            else:
+            elif set_count[0].text == "-":
                 set_count = (player2, player1)
+            else:
+                set_count = (int(set_count[0].text), int(set_count[1].text))
+                if set_count[0] > set_count[1]:
+                    set_count = (player1, player2)
+                else:
+                    set_count = (player2, player1)
             match_queue.append(set_count)
         elif in_match:
             in_match = False
@@ -104,11 +128,16 @@ def apply_matches(matches, ratings):
 def display_results(data, safety=0):
     data.sort(key=lambda x: x[1].mu - x[1].sigma*safety, reverse=True)
     for i in range(len(data)):
-        print("{0:>2}: {1:<9} ({2:.2f})".format(i+1, data[i][0], data[i][1].mu - data[i][1].sigma*safety))
+        print("{0:>3}: {1:<23} ({2:.2f})".format(i+1, data[i][0], data[i][1].mu - data[i][1].sigma*safety))
 
 def date_from_text(text):
-    attr = text.split("-")
-    return datetime.date(int(attr[0]), int(attr[1]), int(attr[2]))
+    if "-" in text:
+        attr = text.split("-")
+        return datetime.date(int(attr[0]), int(attr[1]), int(attr[2]))
+    else:
+        attr= text.split("/")
+        return datetime.date(int(attr[2]), int(attr[0]), int(attr[1]))
+
 
 if __name__ == "__main__":
     env = TrueSkill(draw_probability=0)
@@ -120,10 +149,9 @@ if __name__ == "__main__":
     else:
         driver = get_driver()
         for i in range(len(ratings)):
-            for j in range(i+1, len(ratings)):
-                inputs = get_inputs(driver)
-                input_values(driver, inputs, players[i], players[j])
-                get_matches(driver, matches, i, j)         
+            inputs = get_inputs(driver)
+            input_values(driver, inputs, players[i])
+            get_matches(driver, matches, i)         
         driver.quit()
         store_matches(matches)
 
