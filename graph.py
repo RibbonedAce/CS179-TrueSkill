780cd3820b889656fc8d4126b0e990b914ab05c8
John Choi <jchoi2@coxautoinc.com>
2019-06-04 16:29:09 -0700

    Fixed labeling for graphs

--- graph.py
+++ graph.py
@@ -1,17 +1,14 @@
 import matplotlib.pyplot as plt
-import random
-
-# Random HEX color strings if needed
-# r = lambda: random.randint(0,255)
-# color='#%02X%02X%02X' % (r(),r(),r())
 
 def plot_stats(xs, ys, players):
 	plt.title('TrueSkill Rankings Over Time')
 	plt.ylabel('TrueSkill Ranking')
 	plt.xlabel('Date')
 
+	index = 0
 	for player_stats in ys:
-		plt.plot(xs, player_stats, label=players[ys.index(player_stats)])
+		plt.plot(xs, player_stats, label=players[index])
 		plt.legend()
+		index += 1
 
 	plt.show()
