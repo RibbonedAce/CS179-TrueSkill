ef11fef099ebed3233f13de417f52c6a108088a5
Tyler Gemora <tylergemora@yahoo.com>
2019-06-06 18:57:47 -0700

    Use weights for winners/losers bracket

--- tylertest.py
+++ tylertest.py
@@ -22,7 +22,7 @@ players = ["Hungrybox", "Armada", "Leffen", "Plup", "Mango", \
            "Syrox", "Kalamazhu", "Hax", "Ice", "Ginger", \
            "Michael", "Captain Smuckers", "MikeHaze", "Bladewise", "Junebug", \
            "ChuDat", "Spud", "Santi", "2saint", "Slox", \
-           "Rocky (American Player)", "moky", "Spark", "Squid", "Jerry", \
+           "Rocky (American player)", "moky", "Spark", "Squid", "Jerry", \
            "Professor Pro", "Overtriforce", "Ka-Master", "Darktooth", "Drephen", \
            "Drunksloth", "Stango", "Abate", "Amsah", "Nintendude", \
            "Android", "Boyd", "42nd", "FatGoku", "Sharkz", \
@@ -54,7 +54,7 @@ def get_inputs(driver):
     return inputs
 
 def input_values(driver, inputs, player1):
-    inputs[0].send_keys(players[i])
+    inputs[0].send_keys(player1)
     time.sleep(1)
     ActionChains(driver).send_keys(Keys.ENTER).perform()
     ActionChains(driver).send_keys(Keys.ENTER).perform()
@@ -73,41 +73,88 @@ def get_matches(driver, matches, player1):
         return
     in_match = False
     match_queue = []
+    weight = 1
     for k in range(len(match_table)-1, -1, -1):
+        # If match stats, use them
         if match_table[k].get_attribute("class") == "match-row":
             in_match = True
             set_count = match_table[k].find_elements_by_xpath("./*")
             opponent = set_count[3].text
+            # If not important, ignore weight change, but still record loss
             if not opponent in players:
+                set_count = set_count[1:3]
+                # If game count not recorded
+                if set_count[0].text == "-":
+                    weight *= 0.5
+                elif set_count[0].text != "W":
+                    # If game count recorded, winner has higher number
+                    set_count = (int(set_count[0].text), int(set_count[1].text))
+                    if set_count[0] < set_count[1]:
+                        weight *= 0.5
                 continue
+            # Prevents repeated match records
             player2 = players.index(opponent)
             if player2 <= player1:
                 continue
             set_count = set_count[1:3]
+            # If game count not recorded
             if set_count[0].text == "W":
-                set_count = (player1, player2)
+                set_count = (player1, player2, weight)
             elif set_count[0].text == "-":
-                set_count = (player2, player1)
+                set_count = (player2, player1, weight)
+                weight *= 0.5
             else:
+                # If game count recorded, winner has higher number
                 set_count = (int(set_count[0].text), int(set_count[1].text))
                 if set_count[0] > set_count[1]:
-                    set_count = (player1, player2)
+                    set_count = (player1, player2, weight)
                 else:
-                    set_count = (player2, player1)
+                    set_count = (player2, player1, weight)
+                    weight *= 0.5
             match_queue.append(set_count)
         elif in_match:
             in_match = False
+            # Get current date of tournament
             date = date_from_text(match_table[k].find_elements_by_xpath("./*")[0].text)
-            for match in match_queue:
-                matches.append((match[0], match[1], date))
+            for i in range(len(match_queue) - 2):
+                match_weight = match_queue[i][2]
+                # Speical cases for different bracket tournaments
+                if date == date_from_text("2018-05-06") or date == date_from_text("2018-11-18"):
+                    match_weight = 0.5
+                matches.append((match_queue[i][0], match_queue[i][1], match_weight, date))
+            if len(match_queue) >= 2:
+                match1 = match_queue[len(match_queue)-2]
+                match2 = match_queue[len(match_queue)-1]
+                # If last 2 matches are in grand finals, weigh differently
+                if match1[:2] == match2[:2] or match1[:2] == (match2[1], match2[0]):
+                    matches.append((match1[0], match1[1], 0.5, date))
+                    matches.append((match2[0], match2[1], 1, date))
+                else:
+                    match_weight = match1[2]
+                    if date == date_from_text("2018-05-06") or date == date_from_text("2018-11-18"):
+                        match_weight = 0.5
+                    matches.append((match1[0], match1[1], match_weight, date))
+                    match_weight = match2[2]
+                    if date == date_from_text("2018-05-06") or date == date_from_text("2018-11-18"):
+                        match_weight = 0.5
+                    matches.append((match2[0], match2[1], match_weight, date))
+            # If too few matches, weigh like normal
+            else:
+                for i in range(len(match_queue)):
+                    match_weight = match_queue[i][2]
+                    # Speical cases for different bracket tournaments
+                    if date == date_from_text("2018-05-06") or date == date_from_text("2018-11-18"):
+                        match_weight = 0.5
+                    matches.append((match_queue[i][0], match_queue[i][1], match_weight, date))
             match_queue = []
+            weight = 1
 
 def get_matches_csv(file_name="matches.csv"):
     result = []
     with open(file_name, mode="r") as match_file:
         reader = csv.reader(match_file, delimiter=",")
         for row in reader:
-            result.append((int(row[0]), int(row[1]), date_from_text(row[2])))
+            result.append((int(row[0]), int(row[1]), float(row[2]), date_from_text(row[3])))
     return result
 
 def store_matches(matches, file_name="matches.csv"):
@@ -116,25 +163,25 @@ def store_matches(matches, file_name="matches.csv"):
             writer = csv.writer(match_file, delimiter=",", quotechar="\"", quoting=csv.QUOTE_MINIMAL)
             matches.sort(key=lambda x: min(x[0], x[1]), reverse=True)
             matches.sort(key=lambda x: x[0] + x[1], reverse=True)
-            matches.sort(key=lambda x: x[2])
+            matches.sort(key=lambda x: x[3])
             writer.writerows(matches)
     except:
         print("File already open")
 
-def apply_matches(matches):
+def apply_matches(matches, use_weights=False):
     result = []
     ratings = [Rating() for p in players]
-    current_date = matches[0][2]
+    current_date = matches[0][3]
     for match in matches:
-        if current_date != match[2]:
+        if current_date != match[3]:
             result.append(ratings)
             ratings = ratings[:]
-            current_date = match[2]
-        new_r1, new_r2 = rate_1vs1(ratings[match[0]], ratings[match[1]])
-        ratings[match[0]] = new_r1
-        ratings[match[1]] = new_r2
+            current_date = match[3]
+        new_r1, new_r2 = rate([(ratings[match[0]],), (ratings[match[1]],)], weights=[(1,), (match[2] if use_weights else 1,)])
+        ratings[match[0]] = new_r1[0]
+        ratings[match[1]] = new_r2[0]
     result.append(ratings)
-    num_dates = len(unique_dates([m[2] for m in matches]))
+    num_dates = len(unique_dates([m[3] for m in matches]))
     date_result = [[None for d in range(num_dates)] for p in players]
     for i in range(len(date_result)):
         for j in range(len(date_result[i])):
@@ -184,14 +231,14 @@ if __name__ == "__main__":
         matches = get_matches_csv()
     else:
         driver = get_driver()
-        for i in range(len(ratings)):
+        for i in range(len(players)):
             inputs = get_inputs(driver)
             input_values(driver, inputs, players[i])
             get_matches(driver, matches, i)         
         driver.quit()
         store_matches(matches)
 
-    ratings_over_time = apply_matches(matches)
-    graph.plot_stats(unique_dates([m[2] for m in matches]), ratings_over_time[::10], players[::10], 3)
+    ratings_over_time = apply_matches(matches, True)
+    graph.plot_stats(unique_dates([m[3] for m in matches]), ratings_over_time[::10], players[::10], 3)
     display_results(list(zip(players, [r[-1] for r in ratings_over_time])), 3)
     
